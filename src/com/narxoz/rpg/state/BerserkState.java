70fc03d38b0ca7de557f2ee2f84c6801bef6e238
kairatzhamalbek-cd <kairat.zhamalbek@narxoz.kz>
2026-04-26 12:53:13 +0600

    add BerserkState damage modifier

---
+++ src/com/narxoz/rpg/state/BerserkState.java
@@ -0,0 +1,36 @@
+package com.narxoz.rpg.state;
+import com.narxoz.rpg.combatant.Hero;
+
+public class BerserkState implements HeroState{
+
+    @Override
+    public String getName() {
+        return "Berserk";
+    }
+
+    @Override
+    public int modifyOutgoingDamage(int basePower) {
+        return (int)(basePower * 1.5);
+    }
+
+    @Override
+    public int modifyIncomingDamage(int rawDamage) {
+        return (int)(rawDamage * 1.3);
+    }
+
+    @Override
+    public void onTurnStart(Hero hero) {
+    }
+
+    @Override
+    public void onTurnEnd(Hero hero) {
+        if (hero.getHp() > hero.getMaxHp() / 2) {
+            hero.setState(new NormalState());
+        }
+    }
+
+    @Override
+    public boolean canAct() {
+        return true;
+    }
+}
